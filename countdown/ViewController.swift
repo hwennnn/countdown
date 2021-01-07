cbbab70326d022874c5933399ad60bf4728ff0a1
hwen <whman63@gmail.com>
2021-01-07 20:49:41 +0800

    init

---
+++ countdown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  countdown
+//
+//  Created by hwen on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
