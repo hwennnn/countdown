1d3ea5d231018a196748b797b5ee8490f09aa485
hwen <whman63@gmail.com>
2021-01-27 17:00:00 +0800

    setup base storyboard

--- Podfile
+++ Podfile
@@ -13,8 +13,6 @@ target 'countdown' do
   # add pods for any other desired Firebase products
   # https://firebase.google.com/docs/ios/setup#available-pods
 
-  pod 'FirebaseUI'
   pod 'Firebase/Auth'
-
 
 end
---
+++ countdown/Controller/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  countdown
+//
+//  Created by hwen on 27/1/21.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
