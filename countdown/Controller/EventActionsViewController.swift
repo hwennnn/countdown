ab9879a7d2634de01c2948db87212223338b9418
hwen <whman63@gmail.com>
2021-01-29 18:12:26 +0800

    added date and time picker

--- countdown/Controller/EventActionsViewController.swift
+++ countdown/Controller/EventActionsViewController.swift
@@ -13,6 +13,9 @@ class EventActionsViewController: UIViewController, EmojiViewDelegate{
    
     @IBOutlet weak var eventTitle: UITextField!
     @IBOutlet weak var emojiField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var isIncludeTime: UISwitch!
+    @IBOutlet weak var timePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,14 @@ class EventActionsViewController: UIViewController, EmojiViewDelegate{
         emojiView.translatesAutoresizingMaskIntoConstraints = false
         emojiView.delegate = self
         emojiField.inputView = emojiView
+        
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        
+        if (isIncludeTime.isOn){
+            self.timePicker.isHidden = true
+        }else{
+            self.timePicker.isHidden = false
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -45,5 +56,13 @@ class EventActionsViewController: UIViewController, EmojiViewDelegate{
     // callback when tap dismiss button on keyboard
     func emojiViewDidPressDismissKeyboardButton(_ emojiView: EmojiView) {
         emojiField.resignFirstResponder()
+    }
+    
+    @IBAction func updateIncludeTime(_ sender: Any) {
+        if (isIncludeTime.isOn){
+            self.timePicker.isHidden = true
+        }else{
+            self.timePicker.isHidden = false
+        }
     }
 }
