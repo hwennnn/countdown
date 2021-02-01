c0e25eb9f2b848d7053127d3f06e9c27f7ffe2fb
ZazzyDictionary <zac.hong01@gmail.com>
2021-02-01 15:08:59 +0800

    :bug: fixed update widget feature

--- Countdowns/Views/CountdownWidgetSmall.swift
+++ Countdowns/Views/CountdownWidgetSmall.swift
@@ -39,11 +39,13 @@ struct CountdownWidgetSmall: View {
                     Text(decode((event.emoji))!).foregroundColor(.white)
                     Text(event.name).foregroundColor(.white).font(.subheadline).lineLimit(2)
                 }
-                HStack(alignment: .lastTextBaseline){
+                
+                HStack{
                     VStack{
                         Text(String(days)).font(.title).foregroundColor(.white)
-                        Text(utils.getCountDownDesc(combined)).foregroundColor(.white)
-                    }.padding(.trailing,10)
+                        Text(utils.getCountDownDesc(combined)).foregroundColor(.white).padding(.leading)
+                    }
+                    Spacer()
                 }
                 
                 Text(utils.convertDateToString(date: combined)).foregroundColor(.white)
